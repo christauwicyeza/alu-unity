d276d05462517c2904eb3df37c99e43e1352fb80
My Name <c.uwicyeza@alustudent.com>
2024-09-06 13:58:41 +0200

    up is down, down is up

--- unity-assets_ui/Assets/Scripts/CameraController.cs
+++ unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -1,14 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public Transform player;
     public float angleRotation = 45f;
+    public bool isInverted = true;
 
     private Vector3 distanceFromPlayer = Vector3.zero;
-
     private float rotationX = 9.0f;
     private float rotationY = 0.0f;
+
+    public float minYRotation = -60f;
+    public float maxYRotation = 60f;
 
     private void Awake()
     {
@@ -24,19 +29,35 @@ public class CameraController : MonoBehaviour
     void RotateAroundPlayer()
     {
         rotationX += Input.GetAxis("Mouse X") * angleRotation * Time.deltaTime;
-        rotationY -= Input.GetAxis("Mouse Y") * angleRotation * Time.deltaTime;
-        Quaternion rotation = Quaternion.Euler(0, rotationX, 0);
 
+        if (isInverted)
+        {
+            rotationY += Input.GetAxis("Mouse Y") * angleRotation * Time.deltaTime;
+            Debug.Log("Inverted Mode: Camera moving inverted");
+        }
+        else
+        {
+            rotationY -= Input.GetAxis("Mouse Y") * angleRotation * Time.deltaTime;
+            Debug.Log("Normal Mode: Camera moving normally");
+        }
+
+        rotationY = Mathf.Clamp(rotationY, minYRotation, maxYRotation);
+
+        Quaternion rotation = Quaternion.Euler(rotationY, rotationX, 0);
         transform.rotation = rotation;
 
         transform.position = player.position - (rotation * new Vector3(0, 0, 5));
 
         transform.LookAt(player);
-        transform.rotation = rotation;
     }
 
     void FollowPlayer()
     {
         transform.position = player.transform.position + distanceFromPlayer;
+    }
+
+    public void SetInverted(bool value)
+    {
+        isInverted = value;
     }
 }
