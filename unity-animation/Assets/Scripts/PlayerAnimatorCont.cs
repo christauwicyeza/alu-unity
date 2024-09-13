0af16ad9484fdc9a7cca38a70c8ab3a1df56825f
My Name <c.uwicyeza@alustudent.com>
2024-09-13 12:54:14 +0200

    falling

--- unity-animation/Assets/Scripts/PlayerAnimatorCont.cs
+++ unity-animation/Assets/Scripts/PlayerAnimatorCont.cs
@@ -15,11 +15,12 @@ public class PlayerAnimatorController : MonoBehaviour
 
     private void Update()
     {
-        bool isJumping = Input.GetKeyDown(KeyCode.Space);
-
         bool isRunning = Mathf.Abs(Input.GetAxis("Horizontal")) > 0.1f || Mathf.Abs(Input.GetAxis("Vertical")) > 0.1f;
+        bool isJumping = Input.GetKeyDown(KeyCode.Space);
+        bool isFalling = rb.velocity.y < 0;
 
-        animator.SetBool("isJumping", isJumping);
         animator.SetBool("isRunning", isRunning);
+        animator.SetBool("isJumping", isJumping);
+        animator.SetBool("isFalling", isFalling);
     }
 }
